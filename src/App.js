1bbdd6638964eb0da4a62ddcf2940953bc6323c9
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-01-22 13:08:29 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [characters, setCharacters] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8")
+      .then((res) => {
+        setCharacters(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("err detected");
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((character) => {
+        return <Character key={character.id} character={character} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  color: blue;
+
+  button {
+    color: red;
+    background-color: black;
+  }
+  .buttonDiv {
+    width: 100%;
+    padding: 50px 0;
+    text-align: center;
+    background-color: white;
+    margin-top: 20px;
+  }
+`;
+
+export default function Character(props) {
+  const { character } = props;
+  return (
+    <StyledWrapper Id="character-info">
+      <h2>{character.name}</h2>
+
+      <button
+        onClick={() => {
+          const buttonDiv = document.querySelector(".buttonDiv");
+          if (buttonDiv.style.display === "none") {
+            buttonDiv.style.display = "block";
+          } else {
+            buttonDiv.style.display = "none";
+          }
+        }}
+      >
+        More Info
+      </button>
+      <div className="buttonDiv">
+        <p>Gender: {character.gender}</p>
+        <img src={character.image} alt="Rick and Morty character" />
+        <p>Location: {character.location.name}</p>
+        <p>Species: {character.species}</p>
+        <p>Status: {character.status}</p>
+        <p>Created: {character.created}</p>
+      </div>
+    </StyledWrapper>
+  );
+}
